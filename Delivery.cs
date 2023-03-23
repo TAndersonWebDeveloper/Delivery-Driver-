54e2801313d6fed966dacc8c24f42e20e0076598
Tyler Anderson <anderson.tyler10@gmail.com>
2023-03-23 14:42:43 -0500

    initial commit

---
+++ Delivery.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] float destroyDelay = 0.5f;
+
+
+    bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Collision");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package Picked Up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+
+        }
+        if (other.tag == "Customer" && hasPackage == true)
+        {
+            Debug.Log("Customer Delivered To");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+        }
+
+
+
+    }
+}
